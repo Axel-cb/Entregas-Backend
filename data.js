abfa7eb783ccfdf3823ff3ede0f500745725f264
Axel-cb <axel.cbois91@gmail.com>
2023-02-14 20:04:09 -0300

    ain

--- data.js
+++ data.js
@@ -1,56 +1,120 @@
-class ProductManager{
-    products = []    
-    constructor(){}
+const fs = require("fs")
+
+class ProductManager{    
+    #path = ""
+
+    constructor(path){
+        this.#path = path
+    }
     
-    addProduct(title, description, price, thumbail, code, stock){
+    async getProducts(){
+        try{
+            const products = await fs.promises.readFile(this.#path, "utf-8")
+            return JSON.parse(products)
+        }catch{
+            return []
+        }
+    }
+
+    async getIDs(){
+        let products = await this.getProducts()
+        // Genero un array con todos los id's.
+        let ids = products.map( prods => prods.id)
+        // Saco el id mayor y lo retorno.
+        let mayorID = Math.max(...ids)
+        if (mayorID === -Infinity) {
+            return 0
+        } else {
+            return mayorID
+        }
+    }
+    
+    async addProduct(title, description, price, thumbail, code, stock){
+
+        // Al último ID le sumo 1 y se lo asigno a la propiedad id del objeto.
+        let mayorID = await this.getIDs()
+        
         const product = {
             title,
             description,
-            price: `$ ${price}`,
+            price,
             thumbail,
             code,
             stock,
-            id : this.products.length
+            id : ++mayorID
         }
 
+        let products = await this.getProducts()
         let verificar = Object.values(product)
-        let sameCode = this.products.find( prod => prod.code === code)
+        let sameCode = products.find( prod => prod.code === code)
 
-        if (verificar.includes(undefined) === true){
-            console.log(`El producto ${product.title} NO ha sido cargado, debe completar todos los datos.`)
-        }else if(sameCode){
-            console.log(`El producto ${product.title} NO ha sido cargado ya que la propiedad "code" está repetida, ${sameCode.title} tiene el mismo valor.`)
-        }else{
-            this.products.push(product)
-            console.log(`${product.title} cargado correctamente.`)
+        if (verificar.includes(undefined)){
+            throw new Error(`El producto ${product.title} NO ha sido cargado, debe completar todos los datos.`)
         }
+        if(sameCode){
+            throw new Error(`El producto ${product.title} NO ha sido cargado ya que la propiedad "code" está repetida, ${sameCode.title} tiene el mismo valor.`)
+        }
+    
+        products = [...products, product]
+        console.log(`${product.title} cargado correctamente.`)
+        await fs.promises.writeFile(this.#path, JSON.stringify(products))
     }
 
-    getProducts(){
-        return this.products
+    //Para modificar un producto debemos pasar como primer parámetro el ID, y como segundo parámetro un objeto con las propiedades modificadas.
+    async updateProduct(id, propModify){
+        let products = await this.getProducts()
+        let productModify = products.find(i => i.id === id)
+
+        if (!productModify){
+            throw new Error('No se encontró ningún producto con ese ID.')
+        }
+
+        // Hago una verificación para que no se pueda cambiar la prop ID de ningún producto
+        if (Object.keys(propModify).includes('id')){
+            throw new Error('No es posible modificar el ID de un producto.')
+        }
+
+        // Hago una verificación para que no se pueda cambiar la propiedad 'code' por una que ya exista.
+        if (Object.keys(propModify).includes('code')){
+            let sameCode = products.some(i => i.code === propModify.code)
+            if (sameCode){
+                throw new Error('No es posible modificar la propiedad code por una que ya exista.')
+            }
+        }
+        
+        // Modificamos el producto y filtramos un array sin el producto modificado para agreagarlo y que no se repita.
+        productModify = {...productModify, ...propModify}
+        let newArray = products.filter( prods => prods.id !== id)
+        newArray = [...newArray, productModify]
+        await fs.promises.writeFile(this.#path, JSON.stringify(newArray))
+        console.log('Modificación realizada con éxito.')
     }
 
-    getProductById(id){
-        let element = this.products.find(prod => prod.id === id)
+    async getProductById(id){
+        let products = await this.getProducts()
+        let element = products.find(elem => elem.id === id)
         if (element){
             return element
         } else {
-            return "Not Found"
+            throw new Error('No se encuentra producto con ese ID.')
         }
+    }
+
+    async deleteProduct(id){
+        let products = await this.getProducts()
+        let newArray = products.filter(prods => prods.id !== id)
+        await fs.promises.writeFile(this.#path, JSON.stringify(newArray))
+        console.log('Producto eliminado con éxito')
     }
 }
 
-const manager1 = new ProductManager()
-const manager2 = new ProductManager()
 
-// console.log("----- MANAGER 1 -----")
-manager1.addProduct("cpu gaming", "cpu de alta calidad perfectos para el gaming." , 12000, "ruta de imagen", 0, 10)
-manager1.addProduct("Teclado Hyper X", "Teclado 75% mecanico con luz RGB.", 33000, "ruta de imagen", 1, 15)
-manager1.addProduct("Mouse Logitech", "Mouse Logitech inalámbrico", 19000, "ruta de imagen",2,7)
-console.log(manager1.getProductById(1))
-//console.log(manager1.getProducts())
+async function main(){
+    const manager1 = new ProductManager('./products.json')
+     await manager1.addProduct("Silla Gamer", "Silla gamer muy cómoda de alta calidad.", 43000, "thumbail", "code1", 10)
+    //await manager1.addProduct("Teclado Redragon", "Teclado Redragron 60% ideal para el gaming.", 10000, "thumbail", "code2", 20)
+    // await manager1.addProduct("Procesador Ryzen 5 3500u", "Procesador para computadora.", 69000, "thumbail", "code3", 30)
+    // await manager1.addProduct("Mouse gamer inaámbrico", "Mouse gamer con rgb.", 19000, "thumbail", "code4", 40)
+}
 
-// console.log("----- MANAGER 2 -----")
-// manager2.addProduct("Gorra Adidas", "Gorra Adidas blanca.", 13000, "ruta de imagen", 12, 80)
-// manager2.addProduct("Remera Nike", "Remera Nike negra.", 23000, "ruta de imagen", 52, 40)
-// console.log(manager2.getProducts())
+main()
